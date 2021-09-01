21b7299780abd08ea833f20d4c02fdd7e7104c04
gjayesh083 <88800854+gjayesh083@users.noreply.github.com>
2021-09-01 08:13:08 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,48 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5**9
+1953125
+>>> 3//2
+1
+>>> 7//3
+2
+>>> 7/3
+2.3333333333333335
+>>> 6 == 6
+True
+>>> a=20;
+>>> a+=30
+>>> a%=3
+>>> a
+2
+>>> True * False
+0
+>>> True and False
+False
+>>> True and False
+False
+>>> ((6>3) and (7<4)or (18==3) and (9>3))
+False
+>>> s1 = "Nice to have it "
+>>> s2 = "here"
+>>> s3 = s1+s2
+>>> s3
+'Nice to have it here'
+>>> 
+======================== RESTART: E:/Jayesh/practise.py ========================
+386
+462
+418
+344
+236
+566
+978
+328
+162
+758
+918
+237
+>>> 
+==================================================================== RESTART: E:/Jayesh/practise.py ===================================================================
+{'White', 'Black'}
+>>> 
